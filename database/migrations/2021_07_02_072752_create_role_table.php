dcd4b5e4f59e46a17fc5973ede3464e752cd469e
Soumyajit Ball <soumyajit@capitalnumbers.com>
2021-07-02 13:23:52 +0530

    added new migrations
    new migration file

---
+++ database/migrations/2021_07_02_072752_create_role_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('role', function (Blueprint $table) {
+            $table->id();
+            $table->string('role_name',50);
+            $table->string('role_desc',100);
+            $table->tinyInteger('is_active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('role');
+    }
+}
