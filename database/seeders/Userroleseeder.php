7797ebcc997aa4ddefc92272d537a21c61540b6c
Soumyajit Ball <soumyajit@capitalnumbers.com>
2021-07-08 18:20:30 +0530

    new seeders and modify

---
+++ database/seeders/Userroleseeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class Userroleseeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('user_role')->insert([
+            'user_id' => 1,
+            'username' => 'admin',
+            'role_name' => 'Admin',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::table('user_role')->insert([
+            'user_id' => 2,
+            'username' => 'soumyajit',
+            'role_name' => 'Developer',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+    }
+}
